8cffeade32ea87bd824a34e04d8f6aeff5daed01
Alexandre Ballardin <aleballa08@gmail.com>
2025-01-16 17:50:21 -0300

    .

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 let color = document.getElementById('color');
 let createBtn = document.getElementById('createBtn');
-let list = document.getElementById("list");
+let list = document.getElementById("list"); 
 
 createBtn.onclick = (e) => {
     e.preventDefault();
     let newNote = document.createElement("div")
     newNote.classList.add('note');
     newNote.innerHTML = `
-                <span class="close">x</span>
+                <span class="close">&times</span>
                 <textarea placeholder="Escreva Aqui..." rows="10" cols="30"></textarea>`
     newNote.style.borderColor = color.value;
     list.appendChild(newNote);
 };
+
+document.addEventListener('click', (e)=>{
+    let btnClose = e.target;
+    if (btnClose.classList.contains('close')) {
+        btnClose.parentNode.remove();
+    }
+})
 
 
 
