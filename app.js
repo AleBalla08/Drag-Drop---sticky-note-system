c08710debc3d18617425fe0dd7d3c18e991695b6
Alexandre Ballardin <aleballa08@gmail.com>
2025-01-17 08:56:02 -0300

    final commit

--- app.js
+++ app.js
@@ -20,6 +20,62 @@ document.addEventListener('click', (e)=>{
     }
 })
 
+let cursor = {
+    x: null, // cordenadas do mouse
+    y: null
+}
+
+let note = { //evento de arrastar
+    dom: null, //nota arrastada
+    x: null, //coordenada iniciais
+    y: null
+}
+
+
+
+
+document.addEventListener('mousedown', (event) => {
+    if (event.target.classList.contains('note')) {
+        cursor = {
+            x: event.clientX,
+            y: event.clientY
+        }
+
+        note = {
+            dom: event.target,
+            x: event.target.getBoundingClientRect().left,
+            y: event.target.getBoundingClientRect().top
+        }
+        let currentZindex = event.target.style.zIndex || 0;
+        event.target.style.zIndex = parseInt(currentZindex) + 10;
+    }
+})
+
+document.addEventListener('mousemove', (event) => {
+    if (note.dom == null) return;
+    let currentCursor = {
+        x: event.clientX,
+        y: event.clientY
+    }
+
+    let distance = {
+        x: currentCursor.x - cursor.x,
+        y: currentCursor.y - cursor.y
+    }
+    note.dom.style.cursor = 'grab';
+    note.dom.style.left = (note.x + distance.x) + 'px';
+    note.dom.style.top = (note.y + distance.y) + 'px';
+})
+
+document.addEventListener('mouseup', (event) => {
+    if (note.dom == null) return;
+    note.dom.style.cursor = 'auto';
+    note.dom=null;
+
+
+    
+
+})
 
 
 
